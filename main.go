e386175e9ab2b2f7ea1b45cdab6cee021a961231
CuiHaozhi <cuihaozhi@chinacloud.com.cn>
2018-02-04 07:53:50 +0800

    first build.
    Signed-off-by: CuiHaozhi <cuihaozhi@chinacloud.com.cn>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"net/http"
 	"flag"
 	"time"
@@ -13,6 +12,9 @@ import (
 var interval = flag.Duration("interval", 3 * time.Second, "How long collector interval.")
 
 func main(){
+	fmt.Printf("Mini Metrics Server.\n")
+	defer fmt.Printf("Bye bye.\n")
+	
 	flag.Parse()
 	m := metrics.NewMetrics("123", *interval)
 	http.ListenAndServe("127.0.0.1:9090", m)
